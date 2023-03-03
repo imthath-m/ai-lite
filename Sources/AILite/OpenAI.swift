f18ec8f5667b5ef6d0a188f94e7530745367ebfb
Imthath <imthath.m@hotmail.com>
2023-03-03 22:46:21 +0530

    Refactor to simplify streaming response

--- Sources/AILite/OpenAI.swift
+++ Sources/AILite/OpenAI.swift
@@ -78,42 +78,21 @@ public extension OpenAI {
     let request: URLRequest = try OpenAI.Request
       .completionRequest(prompt: prompt, model: model, wordLimit: wordLimit, creativityScore: creativityScore, stream: true)
       .urlRequest()
-    return AsyncStream { continuation in
-      MIEventHandler(request: request).observe { data in
-        guard let fullSring = String(data: data, encoding: .utf8) else {
-          log("Unable to get string from data")
-          return
-        }
-        for string in fullSring.components(separatedBy: "\n") {
-          guard !string.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else { continue }
-          let newString: String = string.hasPrefix("data: ") ? String(string.dropFirst(6)) : string
-          if newString == "[DONE]" {
-            log("END OF STREAM")
-            return continuation.finish()
-          }
-          guard let jsonData = newString.data(using: .utf8) else {
-            log("Unable to convert back to JSON data - \(string)")
-            return
-          }
-
-          do {
-            let result: TextCompletion = try self.decoder.decode(TextCompletion.self, from: jsonData)
-            continuation.yield(result)
-          } catch {
-            log("\nNO COMPLETION FOUND in string \(string)")
-            log("Error \(error.localizedDescription)")
-          }
-        }
-      }
-    }
+    return streamResponse(from: request)
   }
 
   func chatStream(thread: [Chat], using model: Chat.Model = .gpt, wordLimit: Int16 = 256, creativityScore: Double = 0.5) throws -> ChatStream {
     let request: URLRequest = try OpenAI.Request
       .chatRequest(thread: thread, model: model, wordLimit: wordLimit, creativityScore: creativityScore, stream: true)
       .urlRequest()
-    return AsyncStream { continuation in
-      MIEventHandler(request: request).observe { data in
+    return streamResponse(from: request)
+  }
+}
+
+private extension OpenAI {
+  func streamResponse<AnyDecodable: Decodable>(from urlRequest: URLRequest) -> AsyncStream<AnyDecodable> {
+    AsyncStream { continuation in
+      MIEventHandler(request: urlRequest).observe { data in
         guard let fullString = String(data: data, encoding: .utf8) else {
           log("Unable to get string from data")
           return
@@ -131,7 +110,7 @@ public extension OpenAI {
           }
 
           do {
-            let result: Completion<ChatStreamChoice> = try self.decoder.decode(Completion<ChatStreamChoice>.self, from: jsonData)
+            let result: AnyDecodable = try self.decoder.decode(AnyDecodable.self, from: jsonData)
             continuation.yield(result)
           } catch {
             log("\nNO COMPLETION FOUND in string \(string)")
